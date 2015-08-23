66a5900c66afe67c637e35a8bea305bedcc3c058
JustusJ <JustusJ@mailinator.com>
2015-08-23 16:59:01 +0200

    asd

--- Rakefile
+++ Rakefile
@@ -1,11 +1,27 @@
 require 'rubygems'
 require 'bundler'
 Bundler.require(:default)
-# require "./lib/slim_helper.rb"
+
+class SlimHelper
+	
+	attr_accessor :templates
+	
+	def initialize
+		self.templates = {}
+	end
+	
+end
 
 desc "best build system ever"
 task :build do |t|
-  o = Tilt.new('src/index.html.slim').render#(SlimHelper.new)
+	helper = SlimHelper.new
+	Dir.glob("src/tml/*.slim").each do |f|
+	  o = Tilt.new(f).render
+		id = f.split(".")[0].split("/").last
+		helper.templates[id] = o
+	end
+	
+  o = Tilt.new('src/index.html.slim').render(helper)
   File.open('index.html', 'w+').write(o)
 end
 
--- src/js/base.js
+++ src/js/base.js
@@ -3,30 +3,46 @@ var url = "https://www.reddit.com/r/$$$/search.json?q=site%3Agfycat.com&sort=new
 var x = "cumsluts+cumfetish+cumcoveredfucking+amateurcumsluts+before_after_cumsluts+throatpies+cumonclothes+CumSwap+OhCumOn+CumAgain+cumplay_gifs+RedditorCum+CumOnGlasses+IsThatCUM+teensexcum+FakeCum+girlslickingcum+prematurecumshots+World_of_cum+ManMilk";
 var x2 = "deepthroat+Throatfucking+gag_spit+DeepThroatTears";
 
-var gfycatBase = "http://giant.gfycat.com/$$$.mp4";
+var gfycatBases = ["http://giant.gfycat.com/$$$.mp4", "http://fat.gfycat.com/$$$.mp4"];
 
 function extractGfycatId(u) {
   return u.match(/\/([^\/]+)$/)[1];
 }
 
-function updatePosts(posts) {
-  $(".Container").empty();
-  var fragment = $(document.createDocumentFragment());
-  posts.forEach(function(child) {
-    var id = extractGfycatId(child.data.url);
-    var i = $("<img>").attr("src", child.data.thumbnail);
-      var a = $("<a>").attr("href", gfycatBase.replace("$$$", id)).text(child.data.title);
-    var r = child.data;
-    var c = $("<div>").append(i).append(a);
-    fragment.append(c);
+$(function() {
+  var postTemplate = $("#post").html();
+  Mustache.parse(postTemplate);
+
+  var videoTemplate = $("#video").html();
+  Mustache.parse(videoTemplate);
+
+  $(document).on("click", "[data-gfycatid]", function() {
+    $(".Video").empty();
+    var e = $(this);
+    var urls = gfycatBases.map(function(base) {
+      var url = base.replace("$$$", e.data("gfycatid"));
+      return {url: url};
+    });
+    var r = Mustache.render(videoTemplate, {urls: urls});
+    $(".Video").append(r);
   });
-  $(".Container").append(fragment);
-}
+  
+  
+  function updatePosts(posts) {
+    $(".Container").empty();
+    var fragment = $(document.createDocumentFragment());
+    posts.forEach(function(child) {
+      child.data.gfyCatId = extractGfycatId(child.data.url);
+      var r = Mustache.render(postTemplate, child.data);
+      fragment.append(r);
+    });
+    $(".Container").append(fragment);
+  }
 
-id_cache = {};
+  id_cache = {};
 
-var u = url.replace("$$$", x2);
-
-$.getJSON(u).done(function(d) {
-  updatePosts(d.data.children);
+  var u = url.replace("$$$", x2);
+  $.getJSON(u).done(function(d) {
+    updatePosts(d.data.children);
+  });  
 });
