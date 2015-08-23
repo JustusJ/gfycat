f49db304ded0dbb8ed9aa08e885e2c6c4330bc60
JustusJ <JustusJ@mailinator.com>
2015-08-23 14:28:55 +0200

    asd

--- src/js/base.js
+++ src/js/base.js
@@ -1,4 +1,12 @@
 var url = "https://www.reddit.com/r/deepthroat+Throatfucking+gag_spit+DeepThroatTears/search.json?q=site%3Agfycat.com&sort=new&restrict_sr=on&t=all&limit=100&jsonp=?";
-alert(url);
-var x = $.getJSON(url).done(function(d) { console.log(arguments); });
-console.log(x);
+//var x = $.getJSON(url).done(function(d) { console.log(arguments); });
+//console.log(x);
+
+
+var a = $.ajax({
+    url: url,
+    jsonp : false,
+    jsonpCallback: 'jsonCallback',
+    dataType : 'jsonp',
+    crossDomain: true
+});
