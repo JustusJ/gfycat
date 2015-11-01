a5b5287cc5f09508c5c402ba68f695759f44b1c8
JustusJ <JustusJ@mailinator.com>
2015-11-01 15:29:28 +0100

    asd

---
+++ src/js/subreddits_lists.js
@@ -0,0 +1,37 @@
+var subreddit_lists = {
+  "lists": [{
+    "names": [
+      "cumsluts",
+      "cumfetish",
+      "cumcoveredfucking",
+      "amateurcumsluts",
+      "before_after_cumsluts",
+      "throatpies",
+      "cumonclothes",
+      "CumSwap",
+      "OhCumOn",
+      "CumAgain",
+      "cumplay_gifs",
+      "RedditorCum",
+      "CumOnGlasses",
+      "IsThatCUM",
+      "teensexcum",
+      "FakeCum",
+      "girlslickingcum",
+      "prematurecumshots",
+      "World_of_cum",
+      "ManMilk",
+      "TrueBukkake",
+      "CumFetish"
+    ],
+    "title": "CUM"
+  }, {
+    "names": [
+      "deepthroat",
+      "Throatfucking",
+      "gag_spit",
+      "DeepThroatTears"
+    ],
+    "title": "THROAT"
+  }]
+};
